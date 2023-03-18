6a6440b5f4518d7250730beaa3380540efdcdaa3
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-18 14:20:54 -0400

    feat: another program

---
+++ example_programs/monster_fighting_game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+
+int monsterHealth = 10;
+int heroHealth = 10;
+
+Console.WriteLine("Name your hero: ");
+string name = Console.ReadLine();
+Console.WriteLine("Ready to fight " + name + "!");
+Console.WriteLine("");
+bool herosTurn = true;
+
+while (monsterHealth > 0 && heroHealth > 0)
+{
+    roll = dice.Next(1, 7);
+    if (herosTurn)
+    {
+        monsterHealth -= roll;
+        Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monsterHealth} health.");
+        herosTurn = false;
+    }
+    else
+    {
+        heroHealth -= roll;
+        Console.WriteLine($"{name} was damaged and lost {roll} health and now has {heroHealth} health.");
+        herosTurn = true;
+    }
+    Thread.Sleep(1000);
+}
