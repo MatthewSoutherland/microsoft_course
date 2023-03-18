917c88bf2ea905d9b1baceb82414f3b3ab7e236e
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-18 15:22:47 -0400

    feat: completed string problem

---
+++ example_programs/set_profile_loop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+bool runLoop = true;
+string usersRole = "User";
+while (runLoop)
+{
+    Console.WriteLine($"Enter (A)(M)(U)\n(A)Administrator\n(M)Manager\n(U)User");
+    string role = Console.ReadLine();
+    try
+    {
+        role = role.Trim().ToLower();
+        if (role == "a")
+        {
+            usersRole = "Administrator";
+            runLoop = false;
+        }
+        else if (role == "m")
+        {
+            usersRole = "Manager";
+            runLoop = false;
+        }
+        else if (role == "u")
+        {
+            usersRole = "User";
+            runLoop = false;
+        }
+        else
+        {
+            Console.Write("N"); Thread.Sleep(100); Console.Write("O"); Thread.Sleep(100); Console.Write("T"); Thread.Sleep(100); Console.Write(" "); Thread.Sleep(100); Console.Write("V"); Thread.Sleep(100); Console.Write("A"); Thread.Sleep(100); Console.Write("L"); Thread.Sleep(100); Console.Write("I"); Thread.Sleep(100); Console.Write("D"); Thread.Sleep(100); Console.Write(" "); Thread.Sleep(1000); Console.Write($"FAGGOT!\n");
+        }
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Your value was null!!!!!");
+    }
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine($"Your role is {usersRole}. Thank you!");
