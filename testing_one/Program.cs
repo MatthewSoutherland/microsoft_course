ad5a94124ba7b1f76d228dae43c3e58ff4f5beab
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-23 09:55:58 -0400

    feat: added array_methods program

--- testing_one/Program.cs
+++ testing_one/Program.cs
@@ -1,43 +1,9 @@
 ﻿using System;
 
-// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-
-// decimal total = 0m;
-// string message = "";
-
-// foreach (var value in values)
-// {
-//     decimal number; // stores the TryParse "out" value
-//     if (decimal.TryParse(value, out number))
-//     {
-//         total += number;
-//     }
-//     else
-//     {
-//         message += value;
-//     }
-// }
-
-// Console.WriteLine($"Message: {message}");
-// Console.WriteLine($"Total: {total}");
-
-
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-int result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-// Your code here to set result2
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
-
-
-
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Console.WriteLine(valueArray);
+Array.Reverse(valueArray);
+Console.WriteLine(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
