3243b0c68d1f55cbbb12c2c78818579448e7ff18
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-28 10:10:30 -0400

    feat: add string combine logic

---
+++ examples/toggleString.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+static string ToggleCase(string input)
+{
+
+    return new String(input.Select(c => (char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c))).ToArray());
+}
+
+string result = ToggleCase("Hello WoRlD");
+Console.WriteLine(result);
--- examples/two_string_combine.cs
+++ examples/two_string_combine.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
+/* Take 2 strings s1 and s2 including only letters from a to z. 
+Return a new sorted string, the longest possible, containing distinct 
+letters - each taken only once - coming from s1 or s2. */
 static string Longest(string s1, string s2)
 {
     string s3 = s1 + s2;
