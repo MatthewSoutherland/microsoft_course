fc66282c125d9ee86e265c1dc940047b7105256b
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-29 13:44:19 -0400

    feat: bank account class creation

---
+++ class_creation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testing_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Matt", 5000.00m);
+            Console.WriteLine($"Account {account.number} was created for {account.owner} with a balance of {account.balance}");
+        }
+    }
+}
