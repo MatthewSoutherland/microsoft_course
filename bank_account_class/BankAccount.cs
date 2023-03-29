e7f069fe3e3d9b5cc260c3ae84d8d424722b5853
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-29 16:40:23 -0400

    feat: created bank account program with various classes

---
+++ bank_account_class/BankAccount.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testing_one
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransaction)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int BankAccountSeed = 12345678;
+        private List<Transaction> allTransaction = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            this.Number = BankAccountSeed.ToString();
+            BankAccountSeed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransaction.Add(deposit);
+        }
+        public void MakeWithDrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransaction.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            // table header
+            report.AppendLine("Date\t\tAmount\t\tNote");
+            foreach (var item in allTransaction)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t\t{item.Notes}");
+            }
+            report.AppendLine($"\nRemaining balance: {Balance}");
+            return report.ToString();
+        }
+    }
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
