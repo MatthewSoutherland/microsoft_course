988c82cd25b5e7bf9ada972d4d3c06b67edf32b4
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-03-30 10:20:34 -0400

    new class

--- one/Program.cs
+++ one/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace testing_two
+{
+    public class Student
+    {
+        // Constructor, by placing parameters you ensure values wont be null
+        public Student(string FirstName, string LastName, int Age)
+        {
+            IsEnrolled = true;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student()
+            {
+                FirstName = "Garrett",
+                LastName = "Southerland",
+                Age = 14,
+                IsEnrolled = true
+            };
+            Console.WriteLine($"{s.FirstName}\n{s.LastName}\n{s.Age}\n{s.IsEnrolled}");
+        }
+    }
+}
