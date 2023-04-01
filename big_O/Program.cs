64cc6314c9deed0dec7ff706b3dc4c2e59abeb19
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-04-01 11:01:54 -0400

    feat: n log n

---
+++ big_O/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+static int n_log_n(int n)
+{
+    int y = n;
+    while (n > 1)
+    {
+        n = Math.Floor(n / 2);
+        for (let i = 1; i <= y; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
--- one/Program.cs
+++ one/Program.cs
@@ -1,56 +1,15 @@
 ﻿using System;
 
-/* Create a function that takes a positive integer and returns the next bigger 
-number that can be formed by rearranging its digits. For example:
-
-  12 ==> 21
- 513 ==> 531
-2017 ==> 2071
+/* 
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 */
 
-static long NextBiggerNumber(long n)
+static int Sum(int[] numbers)
 {
-
-    // Convert the integer to a string so we can work with its digits
-    string digits = n.ToString();
-
-    // Start from the right-most digit and find the first digit that is smaller than the digit to its right
-    var i = digits.Length - 2;
-    while (i >= 0 && digits[i] >= digits[i + 1])
-    {
-        i--;
-    }
-
-    if (i < 0)
-    {
-        // If we didn't find any digit to swap, the number can't be rearranged to form a bigger number
-        return -1;
-    }
-
-    // Find the smallest digit to the right of i that is greater than the digit at i
-    var j = digits.Length - 1;
-    while (digits[j] <= digits[i])
-    {
-        j--;
-    }
-
-    // Swap the digits at i and j
-    char[] charArray = digits.ToCharArray();
-    char temp = charArray[i];
-    charArray[i] = charArray[j];
-    charArray[j] = temp;
-    digits = new string(charArray);
-
-    // Reverse the digits to the right of i
-    charArray = digits.Substring(i + 1).ToCharArray();
-    Array.Reverse(charArray);
-    digits = digits.Substring(0, i + 1) + new string(charArray);
-
-    return long.Parse(digits);
+    numbers.Order();
+    foreach (int num in numbers)
+        Console.WriteLine(num);
+    return 0;
 }
-
-
-Console.WriteLine(NextBiggerNumber(1234));
-// 2 numbers = 2 comb
-// 3 numbers (123) = (123, 132, 213, 231, 312, 321) 6 comb
-// 4 numbers (1234) = (1234, 1243, 1324, 1342, 1423, 1432) 24 comb
+Sum(new int[1, 3, 4, 5, 6, 2]);
