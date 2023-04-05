483296a54fef3530f87d1b6b75a3af3a819f3745
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-04-05 10:54:12 -0400

    feat: added syntax to look up all key, values in hash table

--- example_prog/hash.cs
+++ example_prog/hash.cs
@@ -7,3 +7,8 @@ hashtable.Add("key2", "value2");
 
 string value = (string)hashtable["key1"];
 Console.WriteLine(value); // Output: "value1"
+
+foreach (DictionaryEntry entry in hashtable)
+{
+    Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+}
--- one/Program.cs
+++ one/Program.cs
@@ -6,4 +6,10 @@ hashtable.Add("key1", "value1");
 hashtable.Add("key2", "value2");
 
 string value = (string)hashtable["key1"];
+
 Console.WriteLine(value); // Output: "value1"
+
+foreach (DictionaryEntry entry in hashtable)
+{
+    Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+}
