aad1f2ef01b47b192b3de398cedd0d41aada2355
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-04-05 10:49:12 -0400

    feat: added hash table example

--- second/Program.cs
+++ second/Program.cs
@@ -1,20 +1,8 @@
 ﻿using System;
 
-/* Build a function that returns an array of integers from n to 1 where n>0.
-Example : n=5 --> [5,4,3,2,1] */
-
-static int[] ReverseSeq(int n)
+string a = "hello";
+Console.WriteLine($"a.length: {a.Length}");
+for (int i = 0; i <= a.Length; i++)
 {
-    int[] myArr = new int[n];
-
-    for (int i = n, j = 0; i >= 1; i--)
-    {
-        myArr[j] = i;
-        j++;
-    }
-
-    return myArr;
+    Console.WriteLine(a[i]);
 }
-
-// return Enumerable.Range(1, n).Reverse().ToArray();
-// return new int[n].Select(_ => n--).ToArray();
