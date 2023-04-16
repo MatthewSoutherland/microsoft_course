80b3f07a3f92d58c0ee7bd4c5649c1792aaceca6
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-04-16 11:13:56 -0400

    feat: use replace to remove exclamation marks

--- second/Program.cs
+++ second/Program.cs
@@ -1,25 +1,10 @@
 ﻿using System;
-using System.Collections;
 
-static IEnumerable<string> OpenOrSenior(int[][] data)
+// Remove Exclamation marks
+
+static string RemoveExclamationMarks(string s)
 {
-    List<string> myList = new List<string> { };
-    for (int i = 0; i < data.Length; i++)
-    {
-        if (data[i][0] >= 55 && data[i][1] > 7)
-        {
-            myList.Add("Senior");
-        }
-        else
-        {
-            myList.Add("Open");
-        }
-    }
-    return myList;
+    return s.Replace("!", "");
 }
 
-var guy = OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } });
-string myString = String.Join(" ", guy);
-Console.WriteLine(myString);
-
-// return data.Select(member => member[0] >= 55 && member[1] > 7 ? "Senior" : "Open").ToList();
+Console.WriteLine(RemoveExclamationMarks("hello!"));
